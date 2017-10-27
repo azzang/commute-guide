6c1e3e7d51139542e4d2c7b69b17ed54653e4991
Jasim <jasimtaqi@gmail.com>
2017-10-26 18:11:29 -0700

    set display to inline, not initial

--- src/components/GetAnswerButton.js
+++ src/components/GetAnswerButton.js
@@ -14,7 +14,7 @@ class GetAnswerButton extends Component {
     return (
       <button type="button" className="GetAnswerButton button-primary u-full-width"
         onClick={this.handleClick.bind(this)} disabled={this.props.isAnswering}>
-        <span style={this.getStyle('none', 'initial')}>Find Out!</span>
+        <span style={this.getStyle('none', 'inline')}>Find Out!</span>
         <span className="fa fa-circle-o-notch fa-spin"
           style={this.getStyle('inline-block', 'none')}></span>
       </button>
